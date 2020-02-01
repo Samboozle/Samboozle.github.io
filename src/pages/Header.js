2b654d0b83215b93ac75a4a0f9bc1d644f47e3f1
Samboozle <samuel.a.bates@gmail.com>
2020-02-01 15:00:05 -0800

    dependencies installed for routing and styling

---
+++ src/pages/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
+
+export default () => {
+  return(
+    <Jumbotron style={{textAlign: 'center'}}>
+      <h1>Samuel Anthony Bates</h1>
+    </Jumbotron>
+  )
+}
