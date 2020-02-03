e7cfc5a73e3cbc9f8700c467302d3685f0679391
Samboozle <samuel.a.bates@gmail.com>
2020-02-02 22:04:09 -0800

    Updates

---
+++ precache-manifest.b2bcc80a4d8af5078566dc33ae93b44e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f1fc190a08975db0181fbbba558f42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13f8625e7aa25676c35",
+    "url": "/static/css/main.58eb4a73.chunk.css"
+  },
+  {
+    "revision": "27bf50680734bcde6866",
+    "url": "/static/js/2.15c08ae9.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.15c08ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13f8625e7aa25676c35",
+    "url": "/static/js/main.dd343506.chunk.js"
+  },
+  {
+    "revision": "cd6a5f832bb12a4158e3",
+    "url": "/static/js/runtime-main.2aef1696.js"
+  },
+  {
+    "revision": "a6db55428841b90769c323dfbc07fdd9",
+    "url": "/static/media/Monoid-Bold-Dollar-0-1-l.a6db5542.ttf"
+  },
+  {
+    "revision": "d5e890617d733130ec04aaaf30041ef3",
+    "url": "/static/media/Monoid-Regular-Dollar-0-1-l.d5e89061.ttf"
+  }
+]);
